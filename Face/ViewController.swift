247bbdd8004d1b6eefab8571d9bf5a61ff74d00a
SHUAI SHAO <Shuai@shuaidemacbook-pro.local>
2016-08-05 14:00:34 +0800

    Initial Commit

---
+++ Face/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Face
+//
+//  Created by SHUAI SHAO on 16/8/5.
+//  Copyright © 2016年 SHUAI SHAO. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
